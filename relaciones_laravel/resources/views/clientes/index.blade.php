441728feab20ba93acc95474e8d92d560678e2ec
ruben <rlozanotique@gmail.com>
2023-11-15 17:53:03 -0500

    hoy15

--- relaciones_laravel/resources/views/clientes/index.blade.php
+++ relaciones_laravel/resources/views/clientes/index.blade.php
@@ -1,24 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    <a href="{{ route('clientes.create') }}">Create new client</a>
-
-    @forelse ($cliente as $clientes)
-        <li>
-            <p>{{ $clientes->nombre }}</p>
-
-            {{-- Iterar sobre los usuarios asociados con el rol --}}
-            @if ($clientes->users)
-                @forelse ($clientes->users as $user)
-                    <p>{{ $user->nombre_usuario }}</p>
-                @empty
-                    <p>No hay usuarios asociados a este cliente</p>
-                @endforelse
-            @else
-                <p>No hay usuarios asociados a este cliente</p>
-            @endif
-        </li>
-    @empty
-        <h6>No hay datos</h6>
-    @endforelse
+<a href="{{ route('clientes.create') }}">Create new client</a>
+@forelse ($cliente as $clientes)
+    <li>
+        <p>{{ $clientes->nombre }} -- {{ $clientes->apellido }} -- {{ $clientes->correo }} -- {{ $clientes->telefono }} -- {{ $clientes->nro_doc }} -- {{ $clientes->users ? $clientes->users->nombre_usuario : 'sin nada' }}</p>
+    </li>
+@empty
+    <h6>No hay datos</h6>
+@endforelse
 @endsection
